c4cffb5e0babaa7e4e2534b1d96ca0cfb41ac5cf
Saif ur rehman <saifarshad.6987@gmail.com>
2018-12-27 17:00:19 +0500

    Change the name of database to pos_dexdevs

--- POS/app/includes/connection.php
+++ POS/app/includes/connection.php
@@ -2,7 +2,7 @@
 			$servername = "localhost";
 			$username = "root";
 			$password = "";
-			$dbname = "pos";
+			$dbname = "pos_dexdevs";
 
 			// Create connection
 			$conn = new mysqli($servername, $username, $password, $dbname);
